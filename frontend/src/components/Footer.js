81dfef832e0c27a60651f16e8a78451bc10a7b7d
Giannis Eleftheriou <g.eleutheriou@wedia.gr>
2024-10-31 02:10:39 +0200

    Final naming fixing and ui

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-black text-white p-4 w-full text-center mt-auto flex items-center justify-between">
-      <p className="text-sm">© Ellenon's Dashboard for <a className='underline' href='https://www.wedia.gr/en'>Wedia</a></p>
+      <p className="text-sm">© Ellenon's Dashboard</p>
       <p className="text-sm"> Thanks to <a className='underline' href='https://wind-ui.com/'>Wind UI</a></p>
     </footer>
   );
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="bg-black text-white p-4 w-full flex justify-between items-center">
         <img
-        src="http://localhost:3456/uploads/wedia-logo.png"
+        src="http://localhost:3456/uploads/je-rl.png"
         alt="Logo"
         className="h-10 mr-4"
       />
